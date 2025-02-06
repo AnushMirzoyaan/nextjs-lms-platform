0eef9a4f0d0ee4479c3195e60643f91ab76a2249
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2025-02-06 18:41:25 +0400

    DB Schema Setup

---
+++ src/drizzle/schema/product.ts
@@ -0,0 +1,25 @@
+import { relations } from "drizzle-orm";
+import { integer, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { CourseProductTable } from "./courseProduct";
+
+export const proroductStatuses = ["public", "private"] as const;
+
+export type ProroductStatus = (typeof proroductStatuses)[number];
+
+export const productStatusEnum = pgEnum("product_status", proroductStatuses);
+
+export const ProductTable = pgTable("productses", {
+  id,
+  name: text().notNull(),
+  description: text().notNull(),
+  imageUrl: text().notNull(),
+  priceInDollars: integer().notNull(),
+  status: productStatusEnum().notNull().default("private"),
+  createdAt,
+  updatedAt,
+});
+
+export const ProductRelationships = relations(ProductTable, ({ many }) => ({
+  courseProducts: many(CourseProductTable),
+}));
